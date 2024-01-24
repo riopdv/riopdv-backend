b523336f6c34a2375dd8c1c609329d793a37e972
Manuella Barros <manubarrossa@gmail.com>
2024-01-23 21:10:45 -0300

    instalando e configurando prisma, .env, bcrypt, zod e jwt

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from '../controllers/app.controller';
 import { AppService } from '../services/app.service';
+import { JwtModule } from '@nestjs/jwt'
+import { JWT_SECRETS } from '../auth/constants'
+import { ConfigModule } from '@nestjs/config'
 
 @Module({
-  imports: [],
+  imports: [
+    JwtModule.register({
+      global: true,
+      secret: JWT_SECRETS,
+      signOptions: { expiresIn: '14d' },
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
